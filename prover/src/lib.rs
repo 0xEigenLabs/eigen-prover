3689a0da99ca0912565c606243aafc3f55aaccf3
Paul Cheng <chengyuejia@foxmail.com>
2023-10-16 22:46:24 +0800

    feat: add prove stage enum

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -3,3 +3,28 @@ mod batch_prove;
 mod c12_prove;
 mod final_stark_prove;
 mod snark;
+
+#[derive(Clone, Debug)]
+pub enum ProveStage {
+    AggProve,
+    BatchProve,
+    C12Prove,
+    FinalStarkProve,
+    SnarkProve,
+}
+
+// impl
+impl ProveStage {
+    fn to_path(&self, task_id: usize) -> String {
+        let prefix = "/tmp";
+
+        let stage_name = match self {
+            Self::AggProve => "agg_proof",
+            Self::BatchProve => "batch_proof",
+            Self::C12Prove => "c12_proof",
+            Self::FinalStarkProve => "final_stark_proof",
+            Self::SnarkProve => "snark_proof",
+        };
+        format!("{prefix}/{task_id}/{stage_name}")
+    }
+}
