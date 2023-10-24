456b803ad69b992e8c9540e429896040207dca0a
Paul Cheng <chengyuejia@foxmail.com>
2023-10-25 00:37:06 +0800

    fmt

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -1,30 +1,29 @@
-#![allow(dead_code)]
 // mod aggregate_prove;
 // mod batch_prove;
 // mod c12_prove;
 // mod final_stark_prove;
 // mod snark;
 
-#[derive(Clone, Debug)]
-pub enum ProveStage {
-    AggProve,
-    BatchProve,
-    C12Prove,
-    FinalStarkProve,
-    SnarkProve,
-}
-
-impl ProveStage {
-    fn to_path(&self, task_id: usize) -> String {
-        let prefix = "/tmp";
-        // let stage_name = match self {
-        //     Self::AggProve => "agg_proof",
-        //     Self::BatchProve => "batch_proof",
-        //     Self::C12Prove => "c12_proof",
-        //     Self::FinalStarkProve => "final_stark_proof",
-        //     Self::SnarkProve => "snark_proof",
-        // };
-        format!("{prefix}/{task_id}")
-    }
-}
+// #[derive(Clone, Debug)]
+// pub enum ProveStage {
+//     AggProve,
+//     BatchProve,
+//     C12Prove,
+//     FinalStarkProve,
+//     SnarkProve,
+// }
+//
+// impl ProveStage {
+//     fn to_path(&self, task_id: usize) -> String {
+//         let prefix = "/tmp";
+//         // let stage_name = match self {
+//         //     Self::AggProve => "agg_proof",
+//         //     Self::BatchProve => "batch_proof",
+//         //     Self::C12Prove => "c12_proof",
+//         //     Self::FinalStarkProve => "final_stark_proof",
+//         //     Self::SnarkProve => "snark_proof",
+//         // };
+//         format!("{prefix}/{task_id}")
+//     }
+// }
 
