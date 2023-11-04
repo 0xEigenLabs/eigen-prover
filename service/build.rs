47de728b0b729cd9fe7311ab6b206224e42c09dc
0x1900matrix <1900matrix@gmail.com>
2023-11-05 01:05:33 +0800

    fmt

--- service/build.rs
+++ service/build.rs
@@ -3,11 +3,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(true)
         .build_client(true)
         // .out_dir("proto")
-        .compile(&["proto/src/proto/statedb/v1/statedb.proto"], &["proto/src/proto/statedb/v1", "proto/include"])?;
+        .compile(
+            &["proto/src/proto/statedb/v1/statedb.proto"],
+            &["proto/src/proto/statedb/v1", "proto/include"],
+        )?;
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
         // .out_dir("proto")
-        .compile(&["proto/src/proto/aggregator/v1/aggregator.proto"], &["proto/src/proto/aggregator/v1", "proto/include"])?;
+        .compile(
+            &["proto/src/proto/aggregator/v1/aggregator.proto"],
+            &["proto/src/proto/aggregator/v1", "proto/include"],
+        )?;
     Ok(())
 }
