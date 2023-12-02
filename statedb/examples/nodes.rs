272309ea5e6ff8aa68ee031725f33d2093392816
eigmax <stephen@ieigen.com>
2023-12-02 17:23:25 +0800

    chore: impl load db and load program bd

--- statedb/examples/nodes.rs
+++ statedb/examples/nodes.rs
@@ -6,14 +6,14 @@ fn main() {
     let res = db.read_program(&key);
     println!("read program: {} {:?}", key, res);
 
-    let value = "value".to_string();
+    let value = "value".chars().map(|c| c as u8).collect::<Vec<_>>();
     let res = db.write_program(&key, &value, false);
     println!("{:?}", res);
 
     let res = db.read_program(&key);
     println!("{:?}", res);
 
-    let value = "value2".to_string();
+    let value = "value2".chars().map(|c| c as u8).collect::<Vec<_>>();
     let res = db.write_program(&key, &value, true);
     println!("{:?}", res);
 
