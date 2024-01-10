1b244446932fd2fa70d3874e259b8dfb78bfd8d2
BC-A <oneyuanju@gmail.com>
2024-01-10 13:06:53 +0800

    chore: opti code

--- service/examples/exec.rs
+++ service/examples/exec.rs
@@ -1,7 +1,5 @@
 use executor_service::executor_service_client::ExecutorServiceClient;
-use executor_service::{ExecutorError, ProcessBatchRequest, TraceConfig};
-
-use std::collections::HashMap;
+use executor_service::{ExecutorError, ProcessBatchRequest};
 
 pub mod executor_service {
     tonic::include_proto!("executor.v1");
@@ -14,8 +12,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let test_file = "../executor/test-vectors/blockInfo.json";
     println!("test_file {}", test_file);
     let batch_l2_data_json = std::fs::read_to_string(test_file).unwrap();
-    let mut request = ProcessBatchRequest::default();
-    request.batch_l2_data = batch_l2_data_json.as_bytes().to_vec();
+    let request = ProcessBatchRequest {
+        batch_l2_data: batch_l2_data_json.as_bytes().to_vec(),
+        ..Default::default()
+    };
     println!("request: {:?}", request);
     let response = executor_client.process_batch(request).await?.into_inner();
     if response.error == ExecutorError::NoError.into() {
