5769629574ea6de5f28f68ca5f25e1ffbe6e9845
eigmax <stephen@ieigen.com>
2024-03-13 01:22:38 +0800

    feat: fill in storage range

--- executor/src/storage_range.rs
+++ executor/src/storage_range.rs
@@ -1,7 +1,11 @@
 use ethers_providers::{Http, Middleware, Provider};
+use futures::future::join_all;
+use std::collections::HashMap;
 use std::sync::Arc;
 
-use ethers_core::types::{GethDebugTracingOptions, H256};
+use ethers_core::types::{Address, BlockId, GethDebugTracingOptions, H256, U256};
+/*
+// It's not all node support debug_traceTransaction
 pub async fn debug_storage_range_at(client: Arc<Provider<Http>>, txid: H256) {
     let options = GethDebugTracingOptions {
         disable_storage: Some(false),
@@ -10,11 +14,37 @@ pub async fn debug_storage_range_at(client: Arc<Provider<Http>>, txid: H256) {
     let trace_tx = client.debug_trace_transaction(txid, options).await.unwrap();
     log::trace!("trace: {:?}", trace_tx);
 }
+*/
+pub fn get_storage(
+    client: Arc<Provider<Http>>,
+    address: Address,
+    block_id: Option<BlockId>,
+    max_slot_num: u64,
+) -> HashMap<H256, H256> {
+    let max_slot_num = if max_slot_num == 0 { 64 } else { max_slot_num };
+    let batch: Vec<_> = (0..max_slot_num)
+        .into_iter()
+        .map(|i| {
+            let pos = H256::from_low_u64_be(i);
+            let f = async {
+                let slot = client.get_storage_at(address, pos, block_id);
+                tokio::join!(slot)
+            };
+            let result = futures::executor::block_on(f);
+            result.0.unwrap()
+        })
+        .collect();
+    (0..max_slot_num)
+        .into_iter()
+        .zip(batch.iter())
+        .map(|(k, v)| (H256::from_low_u64_be(k), *v))
+        .collect()
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ethers_core::types::Address;
+    /*
     #[tokio::test]
     async fn test_trace_tx() {
         let url = "https://polygon.llamarpc.com";
@@ -26,6 +56,7 @@ mod tests {
             .unwrap();
         debug_storage_range_at(client, txid).await;
     }
+    */
 
     #[tokio::test]
     async fn test_storage_range() {
@@ -33,11 +64,10 @@ mod tests {
         let client = Provider::<Http>::try_from(url).unwrap();
         let client = Arc::new(client);
 
-        let from: Address = "0xE592427A0AEce92De3Edee1F18E0157C05861564".parse().unwrap();
-        //let txid = "0x37d934bac59d3bcaac491a565d8015c9b27793898ec93e653d509113c68bdceb".parse().unwrap();
-        let pos = H256::from_low_u64_be(0u64);
-
-        let storage_slots = client.get_storage_at(from, pos, None).await.unwrap();
-        println!("11 {:?}", storage_slots);
+        let from: Address = "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+            .parse()
+            .unwrap();
+        let res = get_storage(client, from, None, 2);
+        println!("get storage: {:?}", res);
     }
 }
