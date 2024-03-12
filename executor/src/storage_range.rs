bddf151e74e0a8657dbc1434af84c27a42b392f6
eigmax <stephen@ieigen.com>
2024-03-12 12:30:02 +0000

    fix: format code

--- executor/src/storage_range.rs
+++ executor/src/storage_range.rs
@@ -1,26 +1,28 @@
-use ethers_providers::{GethDebugTracingOptions, Http, Provider, Middleware};
+use ethers_providers::{Http, Middleware, Provider};
 use std::sync::Arc;
 
-use ethers_core::types::{H256, GethDebugTracingOptions};
-
-pub fn debug_storage_range_at(client: Arc<Provider<Http>>, txid: H256) {
+use ethers_core::types::{GethDebugTracingOptions, H256};
+pub async fn debug_storage_range_at(client: Arc<Provider<Http>>, txid: H256) {
     let options = GethDebugTracingOptions {
         disable_storage: Some(false),
         ..Default::default()
     };
-    let trace_tx = client.debug_trace_transaction(txid, options);
+    let trace_tx = client.debug_trace_transaction(txid, options).await.unwrap();
     log::trace!("trace: {:?}", trace_tx);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_trace_tx() {
-        let url = "http://localhost:8545";
-        let txid = "0xc265f68f4e5a4f83d2120cc5084c955b10241f26bb108a1857cdcdf0fce4f2da";
+    #[tokio::test]
+    async fn test_trace_tx() {
+        let url = "https://mainnet.infura.io/v3/c60b0bb42f8a4c6481ecd229eddaca27";
         let client = Provider::<Http>::try_from(url).unwrap();
         let client = Arc::new(client);
+
+        let txid = "0x215d5a2aa47abaa65f4a81c8b38b5157dee66608a9442c82516925a3b6bb0d84"
+            .parse()
+            .unwrap();
         debug_storage_range_at(client, txid);
     }
 }
