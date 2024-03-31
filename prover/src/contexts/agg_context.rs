113562b8ceaba44fc8682ed7fb18f65cd77c4c12
eigmax <stephen@ieigen.com>
2024-03-31 13:23:46 +0000

    fix: agg struct path

--- prover/src/contexts/agg_context.rs
+++ prover/src/contexts/agg_context.rs
@@ -38,7 +38,7 @@ impl AggContext {
             input,
             input2,
             agg_zkin: format!("{}/proof/{}/agg_zkin.json", basedir, task_id),
-            agg_struct: format!("{}/c12.stark_struct.json", basedir), // should be same as c12
+            agg_struct: format!("{}/{}/c12.stark_struct.json", basedir, task_name), // should be same as c12
             agg_stark: StarkProveArgs::new(basedir, &task_path, &r2_task_name, "GL"),
             agg_circom: CircomCompileArgs::new(basedir, &task_path, &r2_task_name, "GL"),
             prove_data_cache,
