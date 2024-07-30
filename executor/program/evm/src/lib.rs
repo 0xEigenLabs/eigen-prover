a222f1ce6ccc3aea63e0a57fd45d6c9d1d43b56c
BC-A <oneyuanju@gmail.com>
2024-07-30 23:41:10 +0800

    chore: update powdr

--- executor/program/evm/src/lib.rs
+++ executor/program/evm/src/lib.rs
@@ -8,7 +8,7 @@ use revm::{
     },
 };
 //use runtime::{print, get_prover_input, coprocessors::{get_data, get_data_len}};
-use powdr_riscv_runtime::{print, input::get_data_serde};
+use powdr_riscv_runtime::{print, io::read};
 
 use models::*;
 
@@ -30,7 +30,7 @@ pub fn recover_address(private_key: &[u8]) -> Option<Address> {
 
 #[no_mangle]
 fn main() {
-    let suite_json: String = get_data_serde(TEST_CHANNEL);
+    let suite_json: String = io::read(TEST_CHANNEL);
     print!("suite_json: {suite_json}\n");
     let suite = read_suite(&suite_json);
 
